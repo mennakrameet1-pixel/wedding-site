f25d164a7319b360b822aa9e23b811cf3786d8f4
mennakrameet1-pixel <mennakrameet1@gmail.com>
2026-03-18 09:53:05 -0700

    Update wedding.config.ts

--- src/config/wedding.config.ts
+++ src/config/wedding.config.ts
@@ -1,20 +1,18 @@
-import { WeddingConfigType } from "@/interfaces/config"
+import { WeddingConfigType } from "@/interfaces/config";
 
-const weddingConfig: WeddingConfigType = {
-    people: {
-        bride: { firstName:'سماسيمو', lastName: 'Doe' },
-        groom: { firstName: 'ميدو', lastName: 'Smith' }
-    },
-date: {
-    date: '2026-06-15T21:30:00',
-    time: '9:30 PM' 
+export const weddingConfig: WeddingConfigType = {
+  people: {
+    bride: { firstName: 'سماسيمو', lastName: '' },
+    groom: { firstName: 'ميدو', lastName: '' },
   },
-    location: {
-        title: 'قاعة السرايا ',
-        address: 'دمياط الجديدة',
-        link: 'https://www.vancouverdine.com/teahouse/',
-        latLng: '49.3016707,-123.1597673'
-    }
-}
-
-export default weddingConfig
+  date: {
+    date: '2026-06-15T21:30:00',
+    time: '9:30 PM',
+  },
+  location: {
+    title: 'قاعة السرايا',
+    address: 'دمياط الجديدة',
+    link: 'https://maps.google.com',
+    latLng: '31.4395,31.6508'
+  }
+};
